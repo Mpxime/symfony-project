2c1457387bb1a812156e9371cab8e6a6b69a9e27
Maxime Pégourié <m.pegourie95@gmail.com>
2023-07-13 10:33:07 +0200

    first commit

---
+++ src/Entity/OrdersDetails.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\OrdersDetailsRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: OrdersDetailsRepository::class)]
+class OrdersDetails
+{
+   
+    #[ORM\Column]
+    private ?int $quantity = null;
+
+    #[ORM\Column]
+    private ?int $price = null;
+
+    #[ORM\Id]
+    #[ORM\ManyToOne(inversedBy: 'ordersDetails')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?orders $orders = null;
+
+    #[ORM\Id]
+    #[ORM\ManyToOne(inversedBy: 'ordersDetails')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?products $products = null;
+
+
+
+    public function getQuantity(): ?int
+    {
+        return $this->quantity;
+    }
+
+    public function setQuantity(int $quantity): static
+    {
+        $this->quantity = $quantity;
+
+        return $this;
+    }
+
+    public function getPrice(): ?int
+    {
+        return $this->price;
+    }
+
+    public function setPrice(int $price): static
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function getOrders(): ?orders
+    {
+        return $this->orders;
+    }
+
+    public function setOrders(?orders $orders): static
+    {
+        $this->orders = $orders;
+
+        return $this;
+    }
+
+    public function getProducts(): ?products
+    {
+        return $this->products;
+    }
+
+    public function setProducts(?products $products): static
+    {
+        $this->products = $products;
+
+        return $this;
+    }
+}
